921ed29fbae1e4654286958f3da30976dc471619
ydx927850 <452963125@qq.com>
2019-11-26 17:29:53 +0800

    汉明重量

---
+++ src/main/java/algorithm/otherProblem/HammingWeight.java
@@ -0,0 +1,24 @@
+package algorithm.otherProblem;
+
+/**
+ * 编写一个函数，输入是一个无符号整数，返回其二进制表达式中数字位数为 ‘1’ 的个数（也被称为汉明重量）。
+ *
+ * @author Yandx
+ * @version 1.0
+ * @date created on 2019/11/26 17:01
+ */
+public class HammingWeight {
+    private static int hammingWeight(int n) {
+        int count = 0;
+        int mask = 1;
+        for (int i = 0; i < 32; i++) {
+            if ((n & mask) != 0) {
+                count++;
+            }
+            mask = mask << 1;
+        }
+        return count;
+    }
+
+
+}
